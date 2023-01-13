34d291f6a1c29f893fb1905798fca3445812c67e
jhonamg <orion01123581321@gmail.com>
2023-01-13 03:50:32 -0500

    plantilla up

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     // return view('welcome');
     return view('index');
 
-});
+})->name('login');
 Route::get('marcador', function(){
     return view('controlAccess');
 });
